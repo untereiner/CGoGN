0484e2586ee4ce42fa001cd908c8af6ebef5c857
lionel untereiner <lionel.untereiner@inria.fr>
2018-06-01 16:24:34 +0200

    qt5 ready

--- cmake_modules/Qt_5x.cmake
+++ cmake_modules/Qt_5x.cmake
@@ -1,18 +1,17 @@
 
 set(DESIRED_QT_VERSION = CGoGN_DESIRED_QT_VERSION)
-find_package(Qt5Core REQUIRED)
+find_package(Qt5 COMPONENTS Core Widgets Gui OpenGL Xml Svg REQUIRED)
 #Qt5Widgets required for qt5_wrap_ui
-find_package(Qt5Widgets REQUIRED)
 
 # aliases (module Widgets included by default)
-macro(qt_use_modules)
-  qt5_use_modules(${ARGN} Widgets)
-endmacro()
+#macro(qt_use_modules)
+#  target_link_libraries(${ARGN} Qt5::Widgets)
+#endmacro()
 
 # macro that include module need by cgogn
-macro(qt_use_cgogn_modules)
-  qt5_use_modules(${ARGN} Gui OpenGL Xml Svg Widgets)
-endmacro()
+#macro(qt_use_cgogn_modules)
+#  target_link_libraries(${ARGN} Qt5::Gui Qt5::OpenGL Qt5::Xml Qt5::Svg Qt5::Widgets)
+#endmacro()
 
 macro(qt_wrap_cpp)
   qt5_wrap_cpp(${ARGN})
